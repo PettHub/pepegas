d8050ba5595c8f4d028ed0c1f2dde3f481e64901
PettHub <oliver@gallerit.se>
2019-11-08 10:16:35 +0100

    Upprensning och modifiering av labbfilen

--- DIT953-lab1/Volvo240.java
+++ DIT953-lab1/Volvo240.java
@@ -1,13 +1,8 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Car{
 
     public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
     
     public Volvo240(){
         nrDoors = 4;
@@ -17,33 +12,7 @@ public class Volvo240{
         stopEngine();
     }
     
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-    
     public double speedFactor(){
         return enginePower * 0.01 * trimFactor;
     }
