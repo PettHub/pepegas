9ee910b6bd5f9ab1d1c54f9ccd37e58736c8124b
Paint19 <hanna@schaff.se>
2019-11-13 15:59:47 +0100

    second try javadoc mm

---
+++ JavaDocforlab1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Car"},{"p":"<Unnamed>","l":"Movable"},{"p":"<Unnamed>","l":"Saab95"},{"p":"<Unnamed>","l":"test"},{"p":"<Unnamed>","l":"Volvo240"}]
