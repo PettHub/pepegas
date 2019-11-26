08864a8d9051050751367dc280aaef66610d6f9c
PettHub <oliver@gallerit.se>
2019-11-26 10:19:02 +0100

    added good shit to verstad

--- Source/src/Verkstad.java
+++ Source/src/Verkstad.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Verkstad <A>{
+public class Verkstad <A extends Car>{
     int maxLimit;
     List<A> currentCars = new ArrayList<>();
     public Verkstad(int maxLimit) {
@@ -10,15 +10,14 @@ public class Verkstad <A>{
     public void leaveCarAtShop(A bil){
         if (currentCars.size()<maxLimit){
             currentCars.add(bil);
-            Car tmp = (Car) bil;
-            tmp.isOccupied = true;
+            bil.isOccupied = true;
         }
     }
     public A returnVehicle(A carYouWantToGet){
         for (A car : currentCars){
             if (car == carYouWantToGet) {
-                Car tmp = (Car) car;
-                tmp.isOccupied = false;
+                carYouWantToGet.isOccupied = false;
+                currentCars.remove(carYouWantToGet);
                 return car;
             }
         }
